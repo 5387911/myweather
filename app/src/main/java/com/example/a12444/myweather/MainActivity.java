1fd350d8215ee2303626303bd1715024236d6c65
Your Name <you@example.com>
2018-02-13 20:07:57 +0800

    First

--- app/src/main/java/com/example/a12444/myweather/MainActivity.java
+++ app/src/main/java/com/example/a12444/myweather/MainActivity.java
@@ -1,7 +1,7 @@
 package com.example.a12444.myweather;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
